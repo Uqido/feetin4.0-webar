d2e37baf1963dccc378b4fe660679e39d35892d5
Antonio Ruggiero <antonio.ruggiero@uqido.com>
2021-10-01 18:02:20 +0200

    CTCR with playcanvas

---
+++ CTCR/__settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1239533.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 56140215, 56140216, 56140217, 56147607, 56149705, 56150383, 56334753, 56346327, 56500657, 56502926, 56504755, 56518143 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
